ccdb80c868584dc4d5786c9128946a30450730cd
sohailul <sohailulalam@gmail.com>
2022-05-26 16:39:05 +0600

    changes on PORT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = process.env.PORRT || 5000;
+const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors())
